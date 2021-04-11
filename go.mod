59b407ae67e81446618153b91e4c830330169cf1
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-11 00:23:32 -0700

    Install logrus

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/karashiiro/Kaleidoscope
 
 go 1.16
 
-require github.com/webview/webview v0.0.0-20210330151455-f540d88dde4e
+require (
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	github.com/webview/webview v0.0.0-20210330151455-f540d88dde4e
+)
--- main.go
+++ main.go
@@ -5,19 +5,50 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"os"
 
 	"github.com/karashiiro/Kaleidoscope/buildtime"
+	log "github.com/sirupsen/logrus"
 	"github.com/webview/webview"
 )
 
 //go:embed mirror/build
 var mirror embed.FS
 
+func initLogging() (*os.File, error) {
+	log.SetReportCaller(true)
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Create log directory if it does not exist
+	_, err := os.Stat("log/")
+	if os.IsNotExist(err) {
+		os.Mkdir("log/", 0644)
+	}
+
+	// Create and open log file
+	var logFile *os.File
+	logFile, err = os.OpenFile("log/kaleidoscope.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	log.SetOutput(logFile)
+
+	return logFile, nil
+}
+
 func main() {
+	// Set up logger
+	logFile, err := initLogging()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer logFile.Close()
+
 	// Serve from embedded filesystem
 	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
-		panic(err) // TODO
+		log.Fatalln(err)
 	}
 	port := listener.Addr().(*net.TCPAddr).Port
 	go http.Serve(listener, http.FileServer(http.FS(mirror))) // TODO run this with a channel and logging
