a6de048bb44845b4c7d5739b4a5658ff81a5485b
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-11 19:07:43 -0700

    Set up FaceMesh using tutorial code

--- mirror/src/App.tsx
+++ mirror/src/App.tsx
@@ -1,19 +1,10 @@
-import React from "react";
-import logo from "./logo.svg";
 import styles from "./App.module.scss";
+import { FaceMeshHost } from "./components/FaceMeshHost";
 
 function App() {
 	return (
 		<div className={styles.app}>
-			<header className={styles.appHeader}>
-				<img src={logo} className={styles.appLogo} alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a className={styles.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
+			<FaceMeshHost />
 		</div>
 	);
 }
